9faf7f02c4d50dbfbab4f6cafda4d67c7f6bd129
shamilmuhyin <shamilmuhyin@gmail.com>
2023-02-22 15:48:43 +0530

    jwt-security first commmit

---
+++ src/main/java/com/muhyin/Security/entity/User.java
@@ -0,0 +1,23 @@
+package com.muhyin.Security.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
